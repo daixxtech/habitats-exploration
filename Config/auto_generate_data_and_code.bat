b5bac407a3df387fa2b5c0776f0175ac67df8309
daixxtech <daixxtech@163.com>
2021-05-20 20:05:25 +0800

    chore: 更新打包工具及打包参数

--- Config/auto_generate_data_and_code.bat
+++ Config/auto_generate_data_and_code.bat
@@ -1,6 +1,6 @@
 del /s /q "..\Assets\Res\Config\Conf*.json"
 del /s /q "..\Assets\Scripts\Game\Config\Conf*.cs"
 
-..\Tools\excel-translator\ExcelTranslator.exe -e "../Config" -j "../Assets/Res/Config" -c "../Assets/Scripts/Game/Config" -p "Conf" -n "Game.Config"
+..\Tools\excel-translator\ExcelTranslator.exe -e "../Config" -j "../Assets/Res/Config" -c "../Assets/Scripts/Game/Config" -C "C" -E "E" -n "Game.Config"
 
 pause
