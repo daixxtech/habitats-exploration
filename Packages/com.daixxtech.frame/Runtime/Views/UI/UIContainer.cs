3380b5ef56a78bdf671cac7a6e94435ab7259437
daixxtech <daixxtech@163.com>
2021-05-30 19:00:48 +0800

    feat: 修改 UIContainer 的子对象限制

--- Packages/com.daixxtech.frame/Runtime/Views/UI/UIContainer.cs
+++ Packages/com.daixxtech.frame/Runtime/Views/UI/UIContainer.cs
@@ -5,8 +5,8 @@ namespace Frame.Runtime.Views.UI {
     public class UIContainer : MonoBehaviour {
         private GameObject _template;
         private Transform _pool;
-        private List<GameObject> _spares;
-        public List<MonoBehaviour> Children { get; private set; }
+        private List<Component> _spares;
+        public List<Component> Children { get; private set; }
 
         private void Awake() {
             _pool = new GameObject("Pool").AddComponent<RectTransform>();
@@ -15,35 +15,34 @@ namespace Frame.Runtime.Views.UI {
             _template = transform.Find("Template").gameObject;
             _template.SetActive(false);
 
-            _spares = new List<GameObject>();
-            Children = new List<MonoBehaviour>();
+            _spares = new List<Component>();
+            Children = new List<Component>();
         }
 
-        public void SetCount<T>(int count) where T : MonoBehaviour {
+        public void SetCount<T>(int count) where T : Component {
             if (count > Children.Count) {
                 int diff = count - Children.Count;
                 while (_spares.Count > 0 && diff > 0) {
                     int lastIndex = _spares.Count - 1;
-                    GameObject spare = _spares[lastIndex];
+                    Component spare = _spares[lastIndex];
                     _spares.RemoveAt(lastIndex);
-                    spare.SetActive(true);
+                    spare.gameObject.SetActive(true);
                     spare.transform.SetParent(transform);
-                    Children.Add(spare.AddComponent<T>());
+                    Children.Add(spare);
                     --diff;
                 }
                 while (diff > 0) {
                     GameObject instance = Instantiate(_template, transform);
                     instance.SetActive(true);
-                    Children.Add(instance.AddComponent<T>());
+                    Children.Add(instance.GetComponent<T>() ?? instance.AddComponent<T>());
                     --diff;
                 }
             } else if (count < Children.Count) {
                 for (int i = Children.Count - 1; i >= count; i--) {
-                    GameObject surplus = Children[i].gameObject;
-                    surplus.SetActive(false);
+                    Component surplus = Children[i];
+                    surplus.gameObject.SetActive(false);
                     surplus.transform.SetParent(_pool);
                     _spares.Add(surplus);
-                    Destroy(Children[i]);
                     Children.RemoveAt(i);
                 }
             }
