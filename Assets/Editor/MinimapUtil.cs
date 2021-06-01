c492761577566a223f485010b98052609000b15d
daixxtech <daixxtech@163.com>
2021-06-01 18:04:29 +0800

    feat: 增加更新小地图图片资源的编辑器工具

---
+++ Assets/Editor/MinimapUtil.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Editor {
+    public static class MinimapUtil {
+        private const string HABITAT_SCENE_NAME_PREFIX = "Habitat_";
+        private const string TEXTURE_MINIMAP_PATH = "Res/Textures/Minimap";
+
+        [MenuItem("Tools/Minimap/Capture Minimap Image")]
+        public static void CaptureMinimapImage() {
+            Scene scene = SceneManager.GetActiveScene();
+            Debug.Log(scene.name);
+            if (!scene.name.StartsWith(HABITAT_SCENE_NAME_PREFIX)) {
+                Debug.LogError("[MinimapUtil] CaptureMinimapImage: Current active scene is not a habitat.");
+                return;
+            }
+
+            Transform lightTrans = GameObject.Find("DirectionalLight").transform;
+            Vector3 lightAngles = lightTrans.eulerAngles;
+            lightTrans.eulerAngles = new Vector3(95, 0, 0);
+
+            Transform cameraTrans = new GameObject().transform;
+            cameraTrans.parent = GameObject.Find("Scene").transform;
+            cameraTrans.position = new Vector3(0, 64, 0);
+            cameraTrans.eulerAngles = new Vector3(90, 0, 0);
+
+            Camera camera = cameraTrans.gameObject.AddComponent<Camera>();
+            camera.orthographic = true;
+            camera.orthographicSize = 64;
+
+            RenderTexture targetTex = RenderTexture.GetTemporary(512, 512, 8);
+            camera.targetTexture = targetTex;
+            camera.Render();
+            RenderTexture.active = targetTex;
+            Texture2D minimapTex = new Texture2D(512, 512);
+            minimapTex.ReadPixels(new Rect(0, 0, 512, 512), 0, 0);
+            minimapTex.Apply();
+            RenderTexture.active = null;
+            RenderTexture.ReleaseTemporary(targetTex);
+
+            Object.DestroyImmediate(camera.gameObject);
+            lightTrans.eulerAngles = lightAngles;
+
+            string directory = Path.Combine(Application.dataPath, TEXTURE_MINIMAP_PATH);
+            if (!Directory.Exists(directory)) {
+                Directory.CreateDirectory(directory);
+            }
+            string path = Path.Combine(directory, $"tex_minimap_{scene.name.Replace(HABITAT_SCENE_NAME_PREFIX, string.Empty).ToLower()}.png");
+            File.WriteAllBytes(path, minimapTex.EncodeToPNG());
+            EditorUtility.RevealInFinder(path);
+        }
+    }
+}
