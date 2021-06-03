9099925f85c620b90742b2d1153e04f9dccb33f3
daixxtech <daixxtech@163.com>
2021-06-03 19:28:28 +0800

    feat: 增加密林栖息地的配置及资源

---
+++ Assets/Scripts/Game/Views/Scene/HabitatForestSceneHandler.cs
@@ -0,0 +1,46 @@
+using Frame.Runtime.Modules;
+using Frame.Runtime.Modules.Scenes;
+using Game.Config;
+using Game.Modules;
+using Game.Views.UI;
+using UnityEngine;
+
+namespace Game.Views.Scene {
+    [SceneBind(SceneDef.HABITAT_FOREST)]
+    public class HabitatForestSceneHandler : MonoBehaviour {
+        private void Start() {
+            GameObject characterPrefab = AssetModule.Instance.LoadAsset<GameObject>("Scene_UnityChan.prefab");
+            CScene sceneConf = GameSceneModule.Instance.GetCurSceneConf();
+            Vector3 position = new Vector3(sceneConf.birthPointPosition[0], sceneConf.birthPointPosition[1], sceneConf.birthPointPosition[2]);
+            Vector3 angles = new Vector3(sceneConf.birthPointAngles[0], sceneConf.birthPointAngles[1], sceneConf.birthPointAngles[2]);
+            Instantiate(characterPrefab, position, Quaternion.Euler(angles), transform).AddComponent<PlayerController>();
+            GameObject.FindGameObjectWithTag("MainCamera").AddComponent<CameraController>();
+
+            UIModule.Instance.HideUIAll();
+            UIModule.Instance.ShowUI(UIDef.HUD);
+
+            Transform clueRoot = transform.Find("Clues");
+            var clueConfs = ClueModule.Instance.GetCurHabitatClueConfs();
+            int clueCount = clueConfs.Length;
+            GameObject cluePrefab = AssetModule.Instance.LoadAsset<GameObject>("Scene_Clue.prefab");
+            for (int i = 0; i < clueCount; i++) {
+                GameObject clueInstance = Instantiate(cluePrefab, clueRoot);
+                clueInstance.transform.localPosition = new Vector3(clueConfs[i].position[0], clueConfs[i].position[1], clueConfs[i].position[2]);
+                ClueController clueController = clueInstance.AddComponent<ClueController>();
+                clueController.SetInfo(clueConfs[i]);
+            }
+        }
+
+        private void OnEnable() {
+            Facade.Player.OnInteractedClue += ShowClueTips;
+        }
+
+        private void OnDisable() {
+            Facade.Player.OnInteractedClue -= ShowClueTips;
+        }
+
+        private static void ShowClueTips(int clueID) {
+            UIModule.Instance.ShowUI(UIDef.CLUE_TIPS, CClue.Get(clueID));
+        }
+    }
+}
--- Assets/Scripts/Game/Views/Scene/SceneDef.cs
+++ Assets/Scripts/Game/Views/Scene/SceneDef.cs
@@ -2,5 +2,6 @@ namespace Game.Views.Scene {
     public static class SceneDef {
         public const string START = "Start";
         public const string HABITAT_VALLEY = "Habitat_Valley";
+        public const string HABITAT_FOREST = "Habitat_Forest";
     }
 }
